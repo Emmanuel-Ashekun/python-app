3f1eac2806bac8026b3f9154bf10b2cafc261d58
Emmy Devops <ashekunemmanuel@gmail.com>
2025-06-24 18:26:08 -0400

    add to github

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+import datetime
+import socket
+
+
+@app.route("/api/v1/details")
+def details():
+    return jsonify(
+        {
+            "hostname": socket.gethostname(),
+            "time": datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+        }
+    )
+
+
+@app.route("/api/v1/health")
+def health():
+    return jsonify({"status": "up"}), 200
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0")
+
+
+# '/api/v1/details'
+# '/api/v1/health'
