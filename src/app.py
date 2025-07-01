8290bba86cd8984a632c231b8e41558910e781f3
Emmy Devops <ashekunemmanuel@gmail.com>
2025-07-01 12:18:31 -0400

    updated to include monitoring

--- src/app.py
+++ src/app.py
@@ -1,31 +1,25 @@
 from flask import Flask, jsonify
-
-app = Flask(__name__)
+from prometheus_flask_exporter import PrometheusMetrics
 import datetime
 import socket
 
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # <-- Add this line to enable /metrics
 
 @app.route("/api/v1/info")
 def details():
     return jsonify(
         {
             'hostname': socket.gethostname(),
-            'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+            'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
             'message': 'bruv!!',
             'deployed_on': 'kubernetes'
         }
     )
 
-
 @app.route("/api/v1/health")
 def health():
     return jsonify({"status": "up"}), 200
 
-
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0")
-
-
-# '/api/v1/details'we
-# '/api/v1/healthasswwsssawewwwwasqwwqwweesdf
+    app.run(host="0.0.0.0", port=5000)
